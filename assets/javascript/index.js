822da0245b816afed0c7a2ddde5646955b4d70fb
Alicia <Alis@perlas-mbp.attlocal.net>
2017-04-25 21:48:09 -0500

    working with the mail

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -91,3 +91,37 @@
     });
   });
 
+// var settings = {
+//   "async": true,
+//   "crossDomain": true,
+//   "url": "https://api.sparkpost.com/api/v1/transmissions",
+//   "method": "POST",
+//   "headers": {
+//     "content-type": "application/json",
+//     "authorization": "API KEY",
+//   },
+//   "processData": false,
+//   "data": "{\n    \"content\": {\n      \"from\": \"from_email_at_gmail@aliciatapia.com\",\n      \"subject\": \"Thundercats are GO!!!\",\n      \"text\": \"Sword of Omens, give me sight BEYOND sight\"\n    },\n    \"recipients\": [{ \"address\": \"alicia.tapia@gmail.com\" }]\n}"
+// }
+
+// $.ajax(settings).done(function (response) {
+//   console.log(response);
+// });
+
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://api.sparkpost.com/api/v1/transmissions",
+  "method": "POST",
+  "headers": {
+    "content-type": "application/json",
+    "authorization": "b1bfe8e43c581fa60d873ab5eb2bc25c24fcaffe",
+    "cache-control": "no-cache",
+    "postman-token": "b21332f0-76c4-b2b5-8b81-aacd055277c7"
+  },
+  "data": "{\n   \"options\": {\n      \"open_tracking\": true,\n      \"click_tracking\": true\n   },\n   \"substitution_data\": {\n      \"signature\": \"Aicia Tapia\"\n   },\n   \"recipients\": [\n      {\n         \"address\": {\n            \"email\": \"alicia.tapia@gmail.com\"\n         },\n         \"tags\": [\n            \"learning\"\n         ],\n         \"substitution_data\": {\n            \"first_name\": \"Khalid Adil\"\n         }\n      }\n   ],\n   \"content\": {\n      \"from\": {\n        \"from\": \"Alicia Tapia\",\n        \"email\": \"contact@aliciatapia.com\",\n      },\n      \"subject\": \"What a wonderful test email!\",\n      \"text\": \"Hi {{first_name}}\\r\\nYou have just sent your first email through SparkPost!\\r\\nCongratulations,\\r\\n{{signature}}\",\n      \"html\": \"<strong>Hi {{first_name}},<\\/strong><p>You have just sent your first email through SparkPost!<\\/p><p>Congratulations!<\\/p>{{signature}}\"\n   }\n}"
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
